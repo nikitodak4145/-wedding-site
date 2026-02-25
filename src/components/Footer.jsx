53683d460ca00da69265d5a918f45b9de7453fc2
nikitodak4145 <nikitakul25@gmail.com>
2026-02-25 12:44:59 +0300

    ну наконецто

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -18,7 +18,7 @@ function Footer() {
                Сайт разработан <strong>Никитой</strong>
             </p>
             <a 
-              href="https://portfolio-2bad.vercel.app/" 
+              href="https://portfolio-bdeb.vercel.app/projects" 
               target="_blank" 
               rel="noopener noreferrer"
               className="promo-link"
@@ -30,7 +30,7 @@ function Footer() {
           {/* Ссылка на Instagram */}
           <div className="footer-social">
             <a 
-              href="wedding-site-v69i.vercel.app/" 
+              href="https://wedding-site-zz3w.vercel.app/" 
               target="_blank" 
               rel="noopener noreferrer"
             >
@@ -41,7 +41,7 @@ function Footer() {
 
         {/* Копирайт с твоей ссылкой */}
         <p className="copyright">
-          © 2026 by <a href="wedding-site-v69i.vercel.app/" target="_blank" rel="noopener noreferrer">NK</a>. Все права защищены.
+          © 2026 by <a href="https://portfolio-bdeb.vercel.app/projects" target="_blank" rel="noopener noreferrer">NK</a>. Все права защищены.
         </p>
       </div>
     </footer>
